06534280a84fc27104e1bfea75331c952fa1a4ea
Nicolás <nicolmaderamc123@gmail.com>
2026-06-10 16:01:57 -0500

    textos corregidos

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -9,7 +9,7 @@ export const Boton = ({btncolor, fondob}) => {
         <div className={` ${fondob}`} style={{minHeight: "200px"}}>
             <div className=" container text-center pb-2">
                 <h1 className="text-center"style={{ color: "#7d7d7d" }}>Nuestro proyecto</h1>
-                <p>Tras una extensa investigacion encontramos que no se ha explorado del tema del calentamiento global y la industrializacion desde la perspectiva de una comic interactivo, debido a esto desde ECYDA desarollamos nuestro proyecto estrella, un comic digital interactivo.
+                <p className="text-center"style={{ color: "#7d7d7d" }}>Tras una extensa investigacion encontramos que no se ha explorado del tema del calentamiento global y la industrializacion desde la perspectiva de una comic interactivo, debido a esto desde ECYDA desarollamos nuestro proyecto estrella, un comic digital interactivo.
                  Creemos que por medio de este llegaremos mas profundamente a las personas y tendremos una buena influencia en ellas, te invitamos a ver nuestro comic.</p>
                 <button 
                     type="button" 
