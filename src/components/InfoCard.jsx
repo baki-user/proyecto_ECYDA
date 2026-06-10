d2b9b8af08ff180929ffd968421f393c4e7ce251
Nicolás <nicolmaderamc123@gmail.com>
2026-06-10 16:08:08 -0500

    actu

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -3,7 +3,7 @@ import "../stylesheet/InfoCard.css"
 export const InfoCard = ({BgImg}) => {
      return(
       <div className={`container m-8 p-8 ${BgImg}`}>
-        <div className="d-flex align-items-center flex-column">
+        <div className="d-flex align-items-center flex-column mt-4">
          <img src="./Logo.png" alt="Bootstrap"/>
          <h1 className="fixed-center"style={{ color: "#616161" }}>El humo del progreso esta nublando nuestro futuro</h1>
         </div>
