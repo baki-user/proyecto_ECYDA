a1d75c32b714bacf7b9af2239f4d3ff4af887928
Valentina-Atehortua <vatehortua228@gmail.com>
2026-06-17 12:30:17 -0500

    final

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -20,6 +20,8 @@ export const Productos = () => {
         </div>
       </div>
       <div className="div p-5 h-100" width="100">
+        
+      
       <div className="cuadro p-5 d-flex justify-content-center h-50" width="100">
         <div className="container">
           <p className="tex fw-bold position-relative m-5 p-5 m" style={{ color: "#616161"  }}>Bienvenido a la tienda de ECYDA, un espacio donde la creatividad y la innovación se unen para dar vida a mundos digitales únicos. Nos especializamos en el diseño y la venta de escenarios y personajes en 3D, creados con atención al detalle y pensados para enriquecer proyectos de videojuegos, animación, realidad virtual, contenido digital y narrativas interactivas. En ECYDA encontrarás recursos de alta calidad que te permitirán construir experiencias visuales impactantes, desarrollar historias memorables y materializar tus ideas en entornos tridimensionales.</p>
@@ -31,7 +33,7 @@ export const Productos = () => {
         <div className="row">
           <div className="col p-2">
             <div className="card bg-transparent border-0 m-auto" style={{ width: "30rem" }}>
-              <img className="img4 m-auto" width="420" src="./escenariocuarto.png" alt="" />
+              <img className="img4 m-auto" width="420" src="./escenariovalentia.png" alt="" />
               <div className="card-body">
                 <p className="card-text d-flex justify-content-center" style={{ color: "#616161"  }}>ESCENARIOS 3D</p>
               </div>
@@ -39,19 +41,20 @@ export const Productos = () => {
           </div>
           <div className="col p-2">
             <div className="card bg-transparent border-0 m-auto" style={{ width: "30rem" }}>
-              <img className="img4 m-auto" width="440" src="./recurso4.png" alt="" />
+              <img className="img4 m-auto" width="440" src="./Recurso4.png" alt="" />
               <div className="card-body">
                 <p className="card-text d-flex justify-content-center" style={{ color: "#616161"  }}>PERSONAJES 3D</p>
               </div>
             </div>
 
           </div>
-        </div>   
+        </div>
+        
         <section className="p-4">
         <div className="categoria container fw-bold  rounded-3">
           <p className="d-flex justify-content-center h-100 align-items-center text-white fs-5">TIENDA</p>
         </div>
-      </section>     
+      </section>
       </section>
       <section className="p-4">
       </section>   
