42fde64cbbfd0ddccffeb748f702d2043881464d
Nicolás <nicolmaderamc123@gmail.com>
2026-06-17 14:32:30 -0500

    semi lista

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,124 +1,93 @@
-import "../Components/Header.css"
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
 
 export const Tienda = ({ headerColor, header2Color, mitad }) => {
   return (
-    <div className="Header ">
-      <nav class={`navbar navbar-expand-lg  ${headerColor}`} data-bs-theme="dark">
-        <div class="container ">
-          <a class="navbar-brand" href="#">
-            <img src="./logo-blanco.png" alt="Bootstrap" width="60" height="50" />
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item navbar-light ">
-                <a class="nav-link" href="#">Acerca de</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Comic interactivo</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Tienda</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Quienes somos</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <div className="Header">
+      <Header
+        headerColor="bg-green"
+        header2Color="bg-lightColor"
+      />
+      <div className="d-flex justify-content-end">
+        <img className="img2 p-2" width="60" src="./image.png" alt="" />
+      </div>
       <div className={`${mitad}`}>
-
-      </div >
-      <div className={`text-white text-center fs-5 ${header2Color} mt-3`}>Lo normal fue lo primero en morir</div>
-      <div className="p-4"> 
-      <div className="container-sm">
-        <div className="row">
-          <div className="col-6 mb-3">
-            <div class="card">
-              <img src="../suculenta.jpg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Rosa suculenta</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-
-              </div>
-            </div>
-
-
-          </div>
-
-          <div className="col-6 mb-3">
-            <div class="card" >
-              <img src="../dos.jpg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Suculentas interior</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-
-              </div>
-            </div>
-
-          </div>
-
-          <div className="col-6 mb-3">
-            <div class="card" >
-              <img src="interior.jpg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Interior suculenta</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                <col />
-              </div>
-            </div>
-
-          </div>
-          <div className="col-6 mb-3">
-
-            <div class="card" >
-              <img src="../suculenta2.jpg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Suculenta</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                <col />
-              </div>
-            </div>
-          </div>
-           <div className="col-6 mb-3">
-
-            <div class="card" >
-              <img src="../captus.jpg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Suculenta</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                <col />
-              </div>
-            </div>
-            
-          </div>
+      </div>
+      <div className="p-4">
+        <div className="container-sm">
+          <div className="row">
             <div className="col-6 mb-3">
-
-            <div class="card" >
-              <img src="../maceta morada.jpg" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Suculenta</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                <col />
+              <div className="card">
+                <img src="../suculenta.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Rosa suculenta</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
             </div>
-            
+
+            <div className="col-6 mb-3">
+              <div className="card">
+                <img src="../dos.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Suculentas interior</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6 mb-3">
+              <div className="card">
+                <img src="interior.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Interior suculenta</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6 mb-3">
+              <div className="card">
+                <img src="../suculenta2.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Suculenta</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6 mb-3">
+              <div className="card">
+                <img src="../captus.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Suculenta</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6 mb-3">
+              <div className="card">
+                <img src="../maceta morada.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Suculenta</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
+      <Footer
+        headerColor="bg-green"
+      />
     </div>
-</div> 
-
-
-
   )
 }
