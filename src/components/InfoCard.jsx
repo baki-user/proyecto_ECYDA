6e58f9e62e02ca52e7afe19f0e003c8012bf1636
Nicolás <nicolmaderamc123@gmail.com>
2026-06-25 15:11:14 -0500

    cards

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -24,7 +24,7 @@ export const InfoCard = ({BgImg}) => {
     return(
         <div className={`container m-8 p-8 ${BgImg}`}>
             <div className="d-flex align-items-center flex-column mt-4">
-                <img src="./Logo.png" alt="Bootstrap" className="logo-hover fade-in-scroll" />
+                <img src="/Logo.png" alt="Bootstrap" className="logo-hover fade-in-scroll" />
                 <h1 className="fixed-center mt-4 fade-in-scroll" style={{ color: "#616161", transitionDelay: "0.2s" }}>
                     El humo del progreso esta nublando nuestro futuro
                 </h1>
