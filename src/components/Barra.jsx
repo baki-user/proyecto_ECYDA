8ea878879ffa3654bb00a4a46fb09e5501204e55
Nicolás <nicolmaderamc123@gmail.com>
2026-06-25 15:28:22 -0500

    terminada

--- src/components/Barra.jsx
+++ src/components/Barra.jsx
@@ -1,7 +1,7 @@
 import "../stylesheet/Barra.css"
 export const Barra = ({bgIMG}) => {
     return (
-        <div className={`w-100 ${bgIMG}`} style={{ minHeight: "150px" }}>
+        <div className={ `W-100 ${bgIMG}`} style={{ minHeight: "150px", position: "relative", width: "100vw" }}>
             <h5 className="text-center p-4 m-0" style={{ color: "#616161" }}>
                 Somos una marca enfocada en desarrollar un proyecto multimedial transmedia para concienciar, entretener y enseñar acerca del calentamiento goblal y sus consecuencias.
             </h5>
