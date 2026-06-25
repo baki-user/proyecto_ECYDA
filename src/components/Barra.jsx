10b0e652fb316637cecfe33a37b9a0e0cd9f5286
Nicolás <nicolmaderamc123@gmail.com>
2026-06-25 15:18:28 -0500

    lista

--- src/components/Barra.jsx
+++ src/components/Barra.jsx
@@ -1,12 +1,10 @@
 import "../stylesheet/Barra.css"
 export const Barra = ({bgIMG}) => {
     return (
-        <div>
-            <div className="card" style={{ minWidth: "200px" }}>
-                <div className={`card-body ${bgIMG}`} style={{minHeight: "150px"}}>
-                    <h5 className="card-title align-center text-center"style={{ color: "#616161" }}>Somos una marca enfocada en desarrollar un proyecto multimedial transmedia para concienciar, entretener y enseñar acerca del calentamiento goblal y sus consecuencias.</h5>
-                </div>
-            </div>
+        <div className={`w-100 ${bgIMG}`} style={{ minHeight: "150px" }}>
+            <h5 className="text-center p-4 m-0" style={{ color: "#616161" }}>
+                Somos una marca enfocada en desarrollar un proyecto multimedial transmedia para concienciar, entretener y enseñar acerca del calentamiento goblal y sus consecuencias.
+            </h5>
         </div>
     )
 }
